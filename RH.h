b9e3d44d60ea17c218e61226152ec55648fd5ce7
miladorigueto123 <miladorigueto123@gmail.com>
2026-06-30 16:41:56 -0300

    rh.h

--- RH.h
+++ RH.h
@@ -40,3 +40,23 @@ typedef struct chaveComposta{
     char nome[100];
     data dataNascimento;
 }chaveComposta;
+
+funcionario *criar_funcionario(char *nome, int dia, int mes, int ano, char *mae, char *pai, char *endereco, char *telefone);
+
+int comparar_chave(const void *a, const void *b);
+
+int salvar_funcionario(const funcionario *f, int *posicao);
+
+int carregar_funcionario(funcionario *f, int posicao);
+
+void imprimir_funcionario(const funcionario *f);
+
+void imprimir_funcionario_resumido(const funcionario *f);
+
+void rh_inserir_funcionario(void);
+
+void rh_buscar_funcionario(void);
+
+void rh_excluir_funcionario(void);
+
+void rh_listar_intervalo(void);
